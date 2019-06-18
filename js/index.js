0c5aefe29fcf039530b79809df03c631652a35af
huadee1108 <981929654@qq.com>
2019-06-18 20:33:28 +0800

    second

--- js/index.js
+++ js/index.js
@@ -6,8 +6,27 @@ var swiper = new Swiper('.swiper-container', {
 
 $('.logo-right').click(function() {
  $('.top').show();
-})
+});
 
 $('.close').click(function() {
   $('.top').hide();
-})
+});
+
+var list = [];
+for (var i = 1; i< 7; i++) {
+  list.push(i);
+}
+
+changeStatus(1);
+
+function changeStatus(num) {
+  list.forEach(function(item) {
+    if( num == item) {
+     $('#'+item).children("div.line").show();
+    }else {
+      $('#'+item).children("div.line").hide();
+    }
+  })
+  $('.top').hide();
+  document.getElementById('to'+num).scrollIntoView();
+}
